f8cd20373806583b7798d5dd7b8a6b79fd70e7d5
ANJA-HARIVONY <anjaharivoni@gmail.com>
2021-06-13 22:15:59 +0100

    Ajouter Anja harivony

--- resources/views/partial/navbar.blade.php
+++ resources/views/partial/navbar.blade.php
@@ -5,7 +5,7 @@
                 <div class="inline-flex items-center">
                     <div class="w-2 h-2 p-2 mr-2 rounded-full bg-gradient-to-tr from-blue-500 to-blue-600">
                     </div>
-                    <h2 class="block p-2 text-xl font-medium tracking-tighter text-black transition duration-500 ease-in-out transform cursor-pointer hover:text-blueGray-500 lg:text-x lg:mr-8"> Laravel Formation </h2>
+                    <h2 class="block p-2 text-xl font-medium tracking-tighter text-black transition duration-500 ease-in-out transform cursor-pointer hover:text-blueGray-500 lg:text-x lg:mr-8"> Laravel Formation by ANJA-HARIVONY</h2>
                 </div>
             </a>
             <nav class="flex flex-wrap items-center justify-center text-base lg:mr-auto">
@@ -21,7 +21,7 @@
                     </li>
                 </ul>
             </nav>
-            <button class="w-auto px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform bg-blue-600 border-blue-600 rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:b-gblue-700 ">Button</button>
+            <button class="w-auto px-8 py-2 my-2 text-base font-medium text-white transition duration-500 ease-in-out transform bg-blue-600 border-blue-600 rounded-md focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2 hover:b-gblue-700 ">Log in</button>
         </div>
     </div>
 </div>
