252fa8bac5e338b91bb953ffea4174b630af2ad2
ANJA-HARIVONY <anjaharivoni@gmail.com>
2021-07-04 20:36:57 +0100

    depuis medion

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,7 +7,7 @@
             <div class="w-full mx-auto lg:w-1/2">
                 <h1 class="mx-auto mb-12 text-2xl font-semibold leading-none tracking-tighter text-black lg:text-3xl title-font">{{ $post->title }} </h1>
                 <p class="mx-auto text-base font-medium leading-relaxed text-blueGray-700 ">{{ $post->content }}</p>
-                <a href="{{ route('post.update.show',['id'=>$post->id])}}" class="button">Modiifier</a>
+                <a href="{{ route('post.update.show',['id'=>$post->id])}}" class="button">Modifier</a>
                 <a href="{{ route('post.delete',['id'=>$post->id])}}" class="button">Suprimer</a>
             </div>
 </section>
--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -7,16 +7,27 @@
             <div class="w-full mx-auto lg:w-1/2">
                 <h2 class="mx-auto mb-6 text-xl font-semibold leading-none tracking-tighter text-black title-font">Liste des Posts existants</h2>
                 <p class="mx-auto mb-4 text-base font-medium leading-relaxed text-blueGray-700 "> Ici vous allez trouver tout les listes de commentaire existant dans le base de données </p>
-                @foreach($posts as $post)
-                <p class="flex items-center mb-2 font-semibold text-blueGray-700 ">
-                    <span class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 mr-2 rounded-full">
-                        <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-                            <path fill="none" d="M0 0h24v24H0z"></path>
-                            <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
-                        </svg>
-                    </span><a href="{{ route('post.show',['id'=>$post->id])}}">{{ $post->title }} </a>
-                </p>
-                @endforeach
+                <table>
+                    @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <p class="flex items-center mb-2 font-semibold text-blueGray-700 ">
+                                <span class="inline-flex items-center justify-center flex-shrink-0 w-6 h-6 mr-2 rounded-full">
+                                    <svg fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                                        <path fill="none" d="M0 0h24v24H0z"></path>
+                                        <path d="M10 15.172l9.192-9.193 1.415 1.414L10 18l-6.364-6.364 1.414-1.414z"></path>
+                                    </svg>
+                                </span><a href="{{ route('post.show',['id'=>$post->id])}}">{{ $post->title }} </a>
+
+                            </p>
+                        </td>
+                        <td>
+                            {{$post->created_at->format('d/m/y')}}
+                        </td>
+                    </tr>
+                    @endforeach
+                </table>
+
             </div>
         </div>
     </div>
